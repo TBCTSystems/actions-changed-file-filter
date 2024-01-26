4bf7b70c0d402d55b6c5a843013c4b5f766ecbc1
gvegusb <gus.vega@terumobct.com>
2024-01-26 09:31:11 -0800

    printing inputs

--- dist/index.js
+++ dist/index.js
@@ -2009,6 +2009,7 @@ const os = __webpack_require__(87);
  */
 function issueCommand(command, properties, message) {
     const cmd = new Command(command, properties, message);
+    console.log('From issueCommand - CMD: ', cmd)
     process.stdout.write(cmd.toString() + os.EOL);
     console.log('From issueCommand: ', cmd.toString() + os.EOL)
     console.log('From issueCommand- INPUTS: ', command, '---------', properties, '-------', message)
